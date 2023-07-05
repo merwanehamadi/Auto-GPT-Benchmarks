0425f0e71778673a8e137fbcfa7ef34749917691
Merwane Hamadi <merwanehamadi@gmail.com>
2023-07-05 14:36:39 -0700

    Add 'remember multiple ids' challenge
    Signed-off-by: Merwane Hamadi <merwanehamadi@gmail.com>

---
+++ agbenchmark/challenges/memory/m2/remember_multiple_ids_test.py
@@ -0,0 +1,33 @@
+import os
+from pathlib import Path
+from typing import Any, Dict
+
+import pytest
+
+from agbenchmark.challenges.memory.memory import MemoryChallenge
+
+
+class TestRememberMultipleIds(MemoryChallenge):
+    """The first memory challenge"""
+
+    def get_file_path(self) -> str:  # all tests must implement this method
+        return os.path.join(
+            os.path.dirname(__file__), "remember_multiple_ids_data.json"
+        )
+
+    @pytest.mark.depends(
+        name="test_remember_multiple_ids", depends=["test_basic_memory"]
+    )
+    def test_method(self, config: Dict[str, Any]) -> None:
+        self.setup_challenge(config)
+
+        workspace = Path(os.getcwd()) / config["workspace"]
+        files_contents = self.open_files(workspace, self.data.ground.files)
+
+        scores = []
+        for file_content in files_contents:
+            score = self.scoring(file_content, self.data.ground)
+            print("Your score is:", score)
+            scores.append(score)
+
+        assert 1 in scores
