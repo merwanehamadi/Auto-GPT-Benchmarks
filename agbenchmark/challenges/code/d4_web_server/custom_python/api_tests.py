499035e8046122d13c74bbc6f81d41078bf9b497
Merwane Hamadi <merwanehamadi@gmail.com>
2023-07-20 15:17:09 -0700

    Cleanup subprocess

--- agbenchmark/challenges/code/d4_web_server/custom_python/api_tests.py
+++ agbenchmark/challenges/code/d4_web_server/custom_python/api_tests.py
@@ -8,10 +8,10 @@ import requests
 def test_make_request_and_assert() -> None:
     result = make_request_and_assert()
     print(result)
-    expected_result = {"status": "OK"}
+    expected_result = "OK"
     error_message = f"AssertionError: Expected the output to be {expected_result}"
     print(error_message)
-    assert result == expected_result, error_message
+    assert result in expected_result, error_message
 
 
 def make_assertion() -> None:
